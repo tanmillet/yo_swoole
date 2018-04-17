ecfe4a648be4750bc0d67abbf2703a580f9ecaf3
chris <tanchongtao@stosz.com>
2018-04-17 14:59:40 +0800

    update

--- server.php
+++ server.php
@@ -6,46 +6,46 @@
  * Time 10:12
  */
 
-// $serv = new swoole_server("127.0.0.1", 9501);
-//
-// $serv->set(['task_worker_num' => 4]);
-//
-// $serv->on('connect', function ($serv, $fd) {
-//     echo 'Client: Connect.\n';
-// });
-//
-//
-// $serv->on('receive', function ($serv, $fd, $form_id, $data) {
+ $serv = new swoole_server("127.0.0.1", 9501);
+
+ $serv->set(['task_worker_num' => 4]);
+
+ $serv->on('connect', function ($serv, $fd) {
+     echo 'Client: Connect.\n';
+ });
+
+
+ $serv->on('receive', function ($serv, $fd, $form_id, $data) {
 //     $task_id = $serv->task($data);
-//     // $serv->send($fd, 'Server: '.$data);
-// });
+      $serv->send($fd, 'Server: '.$data);
+ });
+
+ $serv->on('close', function ($serv, $fd) {
+     echo 'Client: close.\n';
+ });
+
+ $serv->start();
+
 //
-// $serv->on('close', function ($serv, $fd) {
-//     echo 'Client: close.\n';
-// });
+//$serv = new swoole_server("127.0.0.1", 9501);
 //
-// $serv->start();
-
-
-$serv = new swoole_server("127.0.0.1", 9501);
-
-$serv->set(['task_worker_num' => 4]);
-
-$serv->on('receive', function ($serv, $fd, $form_id, $data) {
-    $task_id = $serv->task($data);
-    echo "Dispath AsyncTask: id=$task_id\n";
-});
-
-//处理异步任务
-$serv->on('task', function ($serv, $task_id, $from_id, $data) {
-    echo "New AsyncTask[id=$task_id]".PHP_EOL;
-    //返回任务执行的结果
-    $serv->finish("$data -> OK");
-});
-
-//处理异步任务的结果
-$serv->on('finish', function ($serv, $task_id, $data) {
-    echo "AsyncTask[$task_id] Finish: $data".PHP_EOL;
-});
-
-$serv->start();
+//$serv->set(['task_worker_num' => 4]);
+//
+//$serv->on('receive', function ($serv, $fd, $form_id, $data) {
+//    $task_id = $serv->task($data);
+//    echo "Dispath AsyncTask: id=$task_id\n";
+//});
+//
+////处理异步任务
+//$serv->on('task', function ($serv, $task_id, $from_id, $data) {
+//    echo "New AsyncTask[id=$task_id]".PHP_EOL;
+//    //返回任务执行的结果
+//    $serv->finish("$data -> OK");
+//});
+//
+////处理异步任务的结果
+//$serv->on('finish', function ($serv, $task_id, $data) {
+//    echo "AsyncTask[$task_id] Finish: $data".PHP_EOL;
+//});
+//
+//$serv->start();
