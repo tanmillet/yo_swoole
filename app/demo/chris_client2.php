5b41cb461142073b394a93ca7c1f9ada0ae0bfb0
chris <tanchongtao@stosz.com>
2018-04-19 16:30:28 +0800

    添加端口

---
+++ app/demo/chris_client2.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\demo;
+
+
+class chris_client2 {
+
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new \swoole_client(SWOOLE_SOCK_TCP);
+    }
+
+    public function connect()
+    {
+        if (!$this->client->connect('127.0.0.1', 9505, 1)) {
+
+            echo "Error {$this->client->errCode}[{$this->client->errMsg}]";
+        }
+
+        fwrite(STDOUT, "请输入消息 Please input msg：");
+        $msg = trim(fgets(STDIN));
+
+        $this->client->send($msg);
+
+        $message = $this->client->recv();
+
+        echo "Get Message From Server:{$message}\n";
+    }
+}
+
+$client_chris = new chris_client();
+$client_chris->connect();
