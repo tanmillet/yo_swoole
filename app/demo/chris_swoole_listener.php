7f74a4511877aeda08230c273023c955fcc57905
chris <tanchongtao@stosz.com>
2018-04-19 16:02:13 +0800

    update

--- app/demo/chris_swoole_listener.php
+++ app/demo/chris_swoole_listener.php
@@ -8,11 +8,19 @@ class chris_swoole_listener {
     {
         $serv = new  \swoole_server('127.0.0.1', 9502);
         $port = $serv->listen("127.0.0.1", 9501, SWOOLE_TCP);
+        $this->serv->on('Receive', [$this, 'onReceive']);
         $serv->start();
         $port->on('receive', function ($serv, $fd, $from_id, $data) {
-            $serv->send($fd, 'Swoole: '.$data);
+            $serv->send($fd, 'Swoole: ' . $data);
             $serv->close($fd);
         });
     }
+
+    public function onReceive(\swoole_server $serv, $fd, $from_id, $data)
+    {
+        echo "{$fd}:{$data}\n";
+        $serv->send($fd, $data);
+    }
 }
+
 $listener = new  chris_swoole_listener();
