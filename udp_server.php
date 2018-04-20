45d91b686e59292b38ef5dc85bd968f5422ef05d
chris <tanchongtao@stosz.com>
2018-04-20 15:57:14 +0800

    update

--- udp_server.php
+++ udp_server.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * Author Terry Lucas
- * Date 18.1.26
- * Time 10:37
- */
 
 $serv = new swoole_server('127.0.0.1', 9502, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
-
 $serv->on('Packet', function ($serv, $data, $clinet_info) {
-    $serv->sendto($clinet_info['address'], $clinet_info['port'], 'Server: '.$data);
+    $serv->sendto($clinet_info['address'], $clinet_info['port'], 'Server: ' . $data);
 });
 
 $serv->start();
