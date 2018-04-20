7b96f820a272488db9ccd7707b904b1c8383ed6a
chris <tanchongtao@stosz.com>
2018-04-20 16:14:15 +0800

    update

--- chris_tcp.php
+++ chris_tcp.php
@@ -1,18 +1,26 @@
 <?php
 //创建一个服务
-$server = new swoole_server("127.0.0.1", 9503);
+$serv = new swoole_server("127.0.0.1", 9503);
 
-$server->on('connect', function ($server, $fd) {
-    echo "connection open: {$fd}\n";
+$serv->set([
+    'reactor_num' => 2, //reactor thread num
+    'worker_num' => 4,    //worker process num 1-4
+    'backlog' => 128,   //listen backlog
+    'max_request' => 10000,
+    'dispatch_mode' => 1,
+]);
+//$reactor_id  线程
+$serv->on('connect', function ($serv, $fd, $reactor_id) {
+    echo "connection open: {$fd} --- {$reactor_id} -- {$fd}\n";
 });
 
-$server->on('receive', function ($server, $fd, $reactor_id, $data) {
-    $server->send($fd, "Swoole: {$data}");
-    $server->close($fd);
+$serv->on('receive', function ($serv, $fd, $reactor_id, $data) {
+    $serv->send($fd, "swoole: {$data} reactor {$reactor_id}");
+    $serv->close($fd);
 });
 
-$server->on('close', function ($server, $fd) {
+$serv->on('close', function ($serv, $fd) {
     echo "connection close: {$fd}\n";
 });
 
-$server->start();
+$serv->start();
